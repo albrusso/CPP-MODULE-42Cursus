b25f0de5f366cf00cbb19e2f2f098068842b3046
42albrusso <42albrusso@gmail.com>
2024-03-05 14:05:48 +0100

    CPP-01/ex00

---
+++ CPP-MODULE-01/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albrusso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 13:38:17 by albrusso          #+#    #+#             */
+/*   Updated: 2024/03/05 13:50:50 by albrusso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie
+{
+private:
+	std::string	name;
+public:
+	Zombie(std::string name);
+	~Zombie(void);
+	void	announce(void);
+};
+
+#endif
---
+++ CPP-MODULE-01/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albrusso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 13:38:04 by albrusso          #+#    #+#             */
+/*   Updated: 2024/03/05 14:05:23 by albrusso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie	*newZombie(std::string name);
+
+void	randomChump(std::string name);
+
+int	main(void)
+{
+	Zombie	*z;
+
+	z = newZombie("Alfredo");
+	z->announce();
+	randomChump("Gilberto");
+	delete z;
+}
