9a41dd6fadbe71177434f3fc74fe8ac39cfae3ef
42albrusso <42albrusso@gmail.com>
2024-03-05 16:02:21 +0100

    working

---
+++ CPP-MODULE-01/ex01/Zombie.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albrusso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 13:38:54 by albrusso          #+#    #+#             */
+/*   Updated: 2024/03/05 14:03:00 by albrusso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string str)
+{
+	this->name = str;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Zombie called " << this->name << " was destroyed" << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
