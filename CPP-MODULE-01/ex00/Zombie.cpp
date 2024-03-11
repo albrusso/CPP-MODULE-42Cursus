789f2a01e4d25bd6370db08a4674aa371ea58b8d
42albrusso <42albrusso@gmail.com>
2024-03-11 15:52:00 +0100

    ex00 completed

--- CPP-MODULE-01/ex00/Zombie.cpp
+++ CPP-MODULE-01/ex00/Zombie.cpp
@@ -6,23 +6,27 @@
 /*   By: albrusso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 13:38:54 by albrusso          #+#    #+#             */
-/*   Updated: 2024/03/05 14:03:00 by albrusso         ###   ########.fr       */
+/*   Updated: 2024/03/11 15:48:26 by albrusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string str)
+Zombie::Zombie(void)
 {
-	this->name = str;
+}
+
+Zombie::Zombie(std::string s)
+{
+	this->_name = s;
 }
 
 Zombie::~Zombie()
 {
-	std::cout << "Zombie called " << this->name << " was destroyed" << std::endl;
+	std::cout << "Zombie called " << this->_name << " was destroyed" << std::endl;
 }
 
 void	Zombie::announce(void)
 {
-	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- CPP-MODULE-01/ex00/Zombie.hpp
+++ CPP-MODULE-01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: albrusso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 13:38:17 by albrusso          #+#    #+#             */
-/*   Updated: 2024/03/05 13:50:50 by albrusso         ###   ########.fr       */
+/*   Updated: 2024/03/11 15:47:54 by albrusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@
 class Zombie
 {
 private:
-	std::string	name;
+	std::string	_name;
 public:
-	Zombie(std::string name);
+	Zombie(void);
+	Zombie(std::string _s);
 	~Zombie(void);
 	void	announce(void);
 };
