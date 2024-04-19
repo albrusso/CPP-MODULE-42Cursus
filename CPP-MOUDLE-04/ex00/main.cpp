d27c35bd8abce1379d5c3d7df317056b135ce7fb
42albrusso <42albrusso@gmail.com>
2024-04-19 12:06:29 +0200

    working

---
+++ CPP-MOUDLE-04/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albrusso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 11:23:03 by albrusso          #+#    #+#             */
+/*   Updated: 2024/04/19 11:32:20 by albrusso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+
+int main() {
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	meta->makeSound();
+	i->makeSound();
+	j->makeSound();
+return 0; }
